4df139ebef26c70a6b4c9e80fcb581f440cf1c6e
sts-CAD-Software <guenther.schindler@sts-development.biz>
2022-06-07 11:46:42 +0200

    Negative object handling

--- Editing/selectionToPolygon.cs
+++ Editing/selectionToPolygon.cs
@@ -1,14 +1,14 @@
 ﻿//-----------------------------------------------------------------------------------
 // PCB-Investigator Automation Script (Synchronous)
 // Created on 09.05.2022
-// Author Fabio.Gruber
+// Author Guenther.Schindler
 // SDK online reference http://manual.pcb-investigator.com/InterfaceDocumentation/index.php
 // SDK http://www.pcb-investigator.com/sdk-participate
-// Updated: 09.05.2022
-// Description: Replace selection by surface.
+// Updated: 31.05.2022 add negative object handling
+// Description: Create Polygon of selection.
 //-----------------------------------------------------------------------------------
 // Favorite scripts can be placed in the ribbon menu. Therefore a unique GUID and a ButtonEnabled state is mandatory:
-// GUID=padToPolygon_637877000111068306
+// GUID=selectionToPolyGon_637877004572433762
 // ButtonEnabled=1   (Button enabled if: 1=Design is loaded, 2=Always, 4=Design contains components, 8=Loaded step is a panel, 16=Element is selected, 32=Component is selected)
 // AutoStart=false
 
@@ -30,28 +30,41 @@ namespace PCBIScript
 {
 	public class PScript : IPCBIScript
 	{
-		public PScript()
-		{
-		}
-
 		public void Execute(IPCBIWindow parent)
 		{
-			IStep step = parent.GetCurrentStep();
-			if(step==null)return;
-			
-			foreach(IODBObject selectedObj in step.GetSelectedElements())
-			{
-			 IPolyClass poly = selectedObj.GetPolygonOutline();
-			 if(poly==null|| poly.GetEdgeCount()==0)continue;
-			 
-			 IODBLayer layer = step.GetLayer(selectedObj.GetParentLayerName()) as IODBLayer;
-			 if(layer==null)continue;
-			 
-			 IODBObject objSurface = poly.GetSurfaceFromPolygon(layer);
-			 selectedObj.ReplaceItemBy(objSurface);
-			}
-			
-			parent.UpdateView(); 
+            IStep step = parent.GetCurrentStep();
+            if(step==null)return;
+            
+            IFilter filter = new IFilter(parent);
+            
+            foreach(IODBObject selectedObj in step.GetSelectedElements())
+            {
+             IPolyClass poly = selectedObj.GetPolygonOutline();
+             if(poly==null|| poly.GetEdgeCount()==0)continue;
+             
+             IODBLayer layer = step.GetLayer(selectedObj.GetParentLayerName()) as IODBLayer;
+             if(layer==null)continue;
+             
+             foreach(ISurfaceSpecificsD surf in poly.SplitInIsleAndHoles(parent, layer))
+             {
+             IODBObject objSurface = filter.CreatePolygon(layer);
+             
+             bool doSetNegative = false;
+             if(!selectedObj.Positive)
+                doSetNegative = true;
+                
+             selectedObj.ReplaceItemBy(objSurface);
+             
+             if(doSetNegative)
+             {
+                surf.Positive = false; 
+             } 
+             objSurface.SetSpecifics(surf);
+             }
+            }
+            
+            
+            parent.UpdateView(); 
 		}
 	}
 }
